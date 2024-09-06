00ef402e588b12de06ad0b8fab6e16449d473984
Eticha Negash <yotod885@gmail.com>
2024-09-06 05:53:57 -0900

    commited

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Examples 1
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
